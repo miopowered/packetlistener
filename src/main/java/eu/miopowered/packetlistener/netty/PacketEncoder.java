ab2188d2b3a4dd81d641cf7dcd2109e13e8399b6
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-07-29 10:00:57 +0200

    Added validation for packet name

--- src/main/java/eu/miopowered/packetlistener/netty/PacketEncoder.java
+++ src/main/java/eu/miopowered/packetlistener/netty/PacketEncoder.java
@@ -7,18 +7,27 @@ import io.netty.handler.codec.MessageToMessageEncoder;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @AllArgsConstructor
 public class PacketEncoder extends MessageToMessageEncoder<Object> {
 
+    private static final String VALIDATE_PACKET = "packet(handshake|status|login|play)(in|out).*";
+
     private PacketListener packetListener;
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, Object packet, List<Object> list) throws Exception {
+    protected void encode(ChannelHandlerContext channelHandlerContext, Object packet, List<Object> list)
+            throws Exception {
+        list.add(packet);
+        String packetName = packet.getClass().getSimpleName().toLowerCase();
+        if (!packetName.matches(VALIDATE_PACKET)) {
+            return;
+        }
+
         WrappedPacket wrappedPacket = WrappedPacket.from(packet);
         if (this.packetListener.validate(wrappedPacket)) {
             this.packetListener.sent().handle(channelHandlerContext, wrappedPacket);
         }
-        list.add(packet);
     }
 }
